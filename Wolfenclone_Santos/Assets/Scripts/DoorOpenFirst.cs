193b90cc50d729b750b7d178cf925ad7088faaed
Von-Onion <rumoto126@gmail.com>
2022-10-10 08:23:25 -0400

    Door sounds

--- Wolfenclone_Santos/Assets/Scripts/DoorOpenFirst.cs
+++ Wolfenclone_Santos/Assets/Scripts/DoorOpenFirst.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class DoorOpenFirst : MonoBehaviour
 {
     public GameObject theDoor;
+    public AudioSource doorFX;
 
+    //When something passes through the object
     void OnTriggerEnter(Collider other)
     {
+        doorFX.Play();
+        //go to theDoor and have the animator play the animation "DoorOpen"
         theDoor.GetComponent<Animator>().Play("DoorOpen");
+        this.GetComponent<BoxCollider>().enabled = false;
+        StartCoroutine(CloseDoor());
+    }
+
+    //IEnumerator must have a time
+    IEnumerator CloseDoor()
+    { //wait, return a new wait time in seconds (in this case 5)
+      //Then play the doorFx, then play the Closedoor animation
+        yield return new WaitForSeconds(5);
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorClose");
+        this.GetComponent<BoxCollider>().enabled = true;
     }
 }
